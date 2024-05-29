22489f6d7b9acd6a10a0e13607906122773d226b
Жеребцов Кирилл <zherebtsovkirill@mail.ru>
2024-05-29 16:40:43 +0300

    Изменение количества здоровья у врага класса EnemySpaceShip (500->550)

--- main.py
+++ main.py
@@ -190,7 +190,7 @@ class EnemySpaceShip(Enemy):
         # Set up parent class
         super().__init__("enemy_space_ship", "enemy_space_ship")
         self.type = 4
-        self.health = 550
+        self.health = 600
         self.enemy_score = 1000
 
 
